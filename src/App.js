1a784056b5b3e911c858ca128bf0c4b4d86d5595
rafinhaa <rafinhaa@me.com>
2021-11-01 22:16:12 -0300

    two commit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import Section from './Section';
+import styles from './styles';
+
+
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
@@ -26,33 +28,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -83,30 +59,17 @@ const App: () => Node = () => {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
+          <Section title="Learn More">
+            Pastel
+          </Section>
+          <Section title="Learn More">
+            Uhm
+          </Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ src/Section.js
@@ -0,0 +1,33 @@
+
+import { View, Text, useColorScheme } from 'react-native';
+import React from 'react';
+import styles from './styles';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}) => {
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.sectionContainer}>
+        <Text
+            style={[
+            styles.sectionTitle,
+            {
+                color: isDarkMode ? Colors.white : Colors.black,
+            },
+            ]}>
+            {title}
+        </Text>
+        <Text
+            style={[
+            styles.sectionDescription,
+            {
+                color: isDarkMode ? Colors.light : Colors.dark,
+            },
+            ]}>
+            {children}
+        </Text>
+        </View>
+    );
+};
+
+export default Section;
